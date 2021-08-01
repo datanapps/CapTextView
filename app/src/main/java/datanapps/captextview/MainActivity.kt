4ba7bdedb0473c9e96cca15732d8eb537d5a8b52
datanapps <datanapps@gmail.com>
2021-08-01 08:16:13 +0800

    commit minor changes

--- app/src/main/java/datanapps/captextview/MainActivity.kt
+++ app/src/main/java/datanapps/captextview/MainActivity.kt
@@ -2,10 +2,21 @@ package datanapps.captextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import datanapps.captextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //set text value
+        binding.text2.setHtmlText(getString(R.string.text2), true)
+
     }
 }
